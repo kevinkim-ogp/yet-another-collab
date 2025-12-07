b89fc408cafc53a8ecfb810926e60629298f5ac1
kevinkim-ogp <kevinkim@open.gov.sg>
2025-12-07 15:34:54 +0800

    feat(frontend): add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import { RichTextEditor } from '@/components/RichTextEditor'
+import { Route, Routes } from 'react-router-dom'
 
-import { createProvider, Y } from './lib/yjs'
+import Home from './pages/Home'
+import Room from './pages/Room'
 
 function App() {
-  const doc = new Y.Doc()
-  const { provider, user } = createProvider('test', doc)
-
   return (
-    <div className="min-h-screen min-w-screen bg-gray-50 flex items-center justify-center">
-      <div className="bg-white p-8 rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold text-gray-900">
-          Real-time collaboration! 🎉
-        </h1>
-        <p className="mt-4 text-gray-600 ">
-          Yet another collaborative text editor!
-        </p>
-        <main className="max-w-4xl mx-auto px-4 py-8 w-[50vw]">
-          <RichTextEditor provider={provider} user={user} />
-        </main>
-      </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/room/:id" element={<Room />} />
+    </Routes>
   )
 }
 
---
+++ src/pages/Room.tsx
@@ -0,0 +1,29 @@
+import { useParams } from 'react-router-dom'
+
+import { RichTextEditor } from '@/components/RichTextEditor'
+import { createProvider, Y } from '@/lib/yjs'
+
+function Room() {
+  const { id } = useParams()
+  const doc = new Y.Doc()
+
+  const { provider, user } = createProvider(id ?? 'test', doc)
+
+  return (
+    <div className="min-h-screen min-w-screen bg-gray-50 flex items-center justify-center">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <h1 className="text-3xl font-bold text-gray-900">
+          Real-time collaboration! 🎉
+        </h1>
+        <p className="mt-4 text-gray-600 ">
+          Yet another collaborative text editor!
+        </p>
+        <main className="max-w-4xl mx-auto px-4 py-8 w-[50vw]">
+          <RichTextEditor provider={provider} user={user} />
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default Room
